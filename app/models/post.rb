075adc0bb35701f4a3d66eb49af4ceea9e4afc20
ChaseWelsh <fseusb@gmail.com>
2022-10-12 11:38:37 -0600

    orpgsmapw

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 1}
+  validates_inclusion_of :category, in: %w[Fiction Non-Fiction], on: :create
+  validates :content, presence: true, length: {minimum: 100}
 end
